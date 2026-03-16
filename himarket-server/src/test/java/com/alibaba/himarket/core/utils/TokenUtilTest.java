ec8e03ad44d96e2d72ad7c37443b223fab3a339f
Mison <liangmingsen@qdama.cn>
2026-03-16 20:56:15 +0800

    fix(auth): make JWT tokens unique per login

---
+++ himarket-server/src/test/java/com/alibaba/himarket/core/utils/TokenUtilTest.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.alibaba.himarket.core.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import cn.hutool.jwt.JWT;
+import cn.hutool.jwt.JWTUtil;
+import com.alibaba.himarket.core.constant.CommonConstants;
+import com.alibaba.himarket.core.constant.JwtConstants;
+import com.alibaba.himarket.support.common.User;
+import com.alibaba.himarket.support.enums.UserType;
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TokenUtilTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+        setStaticField("JWT_SECRET", "unit-test-secret");
+        setStaticField("JWT_EXPIRE_MILLIS", 60_000L);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        setStaticField("JWT_SECRET", null);
+        setStaticField("JWT_EXPIRE_MILLIS", 0L);
+    }
+
+    @Test
+    void shouldGenerateUniqueTokensForSameAdmin() {
+        String firstToken = TokenUtil.generateAdminToken("admin-1");
+        String secondToken = TokenUtil.generateAdminToken("admin-1");
+
+        assertNotEquals(firstToken, secondToken);
+
+        JWT firstJwt = JWTUtil.parseToken(firstToken);
+        JWT secondJwt = JWTUtil.parseToken(secondToken);
+        assertNotEquals(
+                firstJwt.getPayload(JwtConstants.PAYLOAD_JTI),
+                secondJwt.getPayload(JwtConstants.PAYLOAD_JTI));
+
+        User firstUser = TokenUtil.parseUser(firstToken);
+        assertEquals(UserType.ADMIN, firstUser.getUserType());
+        assertEquals("admin-1", firstUser.getUserId());
+
+        User secondUser = TokenUtil.parseUser(secondToken);
+        assertEquals(UserType.ADMIN, secondUser.getUserType());
+        assertEquals("admin-1", secondUser.getUserId());
+        assertEquals("admin-1", secondJwt.getPayload(CommonConstants.USER_ID).toString());
+    }
+
+    private void setStaticField(String fieldName, Object value) throws Exception {
+        Field field = TokenUtil.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(null, value);
+    }
+}
